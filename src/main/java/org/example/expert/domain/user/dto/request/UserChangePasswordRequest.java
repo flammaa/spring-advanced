2fad9efb79e05057af0add1e8e2b338ae6444c46
J kim <kimjiyeong1000@gmail.com>
2025-02-24 22:51:01 +0900

    lv1-3

--- src/main/java/org/example/expert/domain/user/dto/request/UserChangePasswordRequest.java
+++ src/main/java/org/example/expert/domain/user/dto/request/UserChangePasswordRequest.java
@@ -1,6 +1,8 @@
 package org.example.expert.domain.user.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,7 +12,10 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class UserChangePasswordRequest {
 
-    @NotBlank
+    @NotBlank(message = "비밀번호를 입력해주세요")
+    @Size(min = 8, message = "새 비밀번호는 8자 이상이어야 합니다.")
+    @Pattern(regexp = ".*\\d.*", message = "새 비밀번호는 숫자를 포함해야 합니다.")
+    @Pattern(regexp = ".*[A-Z].*", message = "새 비밀번호는 대문자가 포함되어야 합니다.")
     private String oldPassword;
     @NotBlank
     private String newPassword;
