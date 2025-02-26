28b2b60c9adf3b7bdc8ced4afdb0af61ef8d994c
J kim <kimjiyeong1000@gmail.com>
2025-02-26 10:43:40 +0900

    lv2

--- src/main/java/org/example/expert/domain/todo/entity/Todo.java
+++ src/main/java/org/example/expert/domain/todo/entity/Todo.java
@@ -15,6 +15,10 @@ import java.util.List;
 @Entity
 @NoArgsConstructor
 @Table(name = "todos")
+@NamedEntityGraph(
+        name = "Todo.fetchUser",
+        attributeNodes = @NamedAttributeNode("user")
+)
 public class Todo extends Timestamped {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
--- src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
+++ src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
@@ -1,23 +1,30 @@
 package org.example.expert.domain.todo.repository;
 
+import jakarta.persistence.Entity;
 import org.example.expert.domain.todo.entity.Todo;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+
 public interface TodoRepository extends JpaRepository<Todo, Long> {
 
-    @Query("SELECT t FROM Todo t LEFT JOIN FETCH t.user u ORDER BY t.modifiedAt DESC")
+    @EntityGraph(value = "Todo.fetchUser", type = EntityGraph.EntityGraphType.LOAD)
+//    @Query("SELECT t FROM Todo t LEFT JOIN FETCH t.user u ORDER BY t.modifiedAt DESC")
     Page<Todo> findAllByOrderByModifiedAtDesc(Pageable pageable);
 
+    @EntityGraph(value = "Todo.fetchUser", type = EntityGraph.EntityGraphType.LOAD)
     @Query("SELECT t FROM Todo t " +
             "LEFT JOIN FETCH t.user " +
             "WHERE t.id = :todoId")
     Optional<Todo> findByIdWithUser(@Param("todoId") Long todoId);
 
     int countById(Long todoId);
+
 }
